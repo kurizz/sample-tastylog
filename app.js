b9b8309a2f682dea50d4d486ace40a717a7a91de
kurizz <umozon63313@gmail.com>
2023-08-08 01:04:11 +0900

    静的コンテンツ、favicon を配置

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import favicon from "serve-favicon";
 import router from "./routes/index.js"
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.set("view engine", "ejs");
+
+app.use(favicon(path.join(__dirname, "public/favicon.ico")));
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
 app.use("/", router);
+
 app.listen(PORT, () => {
   console.log(`Application listening at :${PORT}`);
 });
