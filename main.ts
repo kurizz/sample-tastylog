b8536f915bcfb03de3e4376030b41b940c286304
kurizz <umozon63313@gmail.com>
2023-08-07 22:00:18 +0900

    main.ts の修正
    * type/express の追加
    * req を使用していないので `_`
    * port の宣言、デバッグ用 console.log の追加

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
+// @deno-types="npm:@types/express"
 import express from "npm:express@4.17.1";
 
 const app = express();
+const port = 8080
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send("welcome to the deno api!");
-})
+});
 
-app.listen(8080);
+app.listen(port, () => {
+  console.log(`app listening at ${port}`);
+});
