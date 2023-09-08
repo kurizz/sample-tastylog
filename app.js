6d2cf9068c847c01a3b420c6f4d6549e5b70c582
kurizz <umozon63313@gmail.com>
2023-09-08 09:09:38 +0900

    ejs に moment, padding を渡せるようにした

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import favicon from "serve-favicon";
+import moment from "moment";
 
 import { application }  from "./lib/log/logger.js";
 import accesslogger  from "./lib/log/accesslogger.js";
 import applicationlogger  from "./lib/log/applicationlogger.js";
 import router from "./routes/index.js";
 import MySqlClient from "./lib/database/client.js";
+import { padding } from "./lib/math/math.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -16,6 +18,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+  res.locals.moment = moment;
+  res.locals.padding = padding;
+  next();
+});
 
 app.use(favicon(path.join(__dirname, "public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
