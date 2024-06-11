0e03f270b4af5985972154ba05e80b25a193b1b9
kurizz <umozon63313@gmail.com>
2024-06-11 09:32:38 +0900

    ログイン画面の追加

--- routes/account.js
+++ routes/account.js
@@ -2,6 +2,11 @@ import express from "express";
 import accountReviews from "./account.reviews.js"
 
 const router = express();
+
+router.get("/login", (req, res, next) => {
+  res.render("./account/login.ejs");
+})
+
 router.use("/reviews", accountReviews)
 
 export default router;
