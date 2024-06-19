a78d08071bd671a2e4a4f8a0814b8f70b312f457
kurizz <umozon63313@gmail.com>
2024-06-19 10:03:39 +0900

    認証機能の実装

--- routes/account.js
+++ routes/account.js
@@ -1,11 +1,19 @@
 import express from "express";
 import accountReviews from "./account.reviews.js"
+import { authenticate, authorize, PRIVILEGE } from "../lib/security/accesscontrol.js";
 
 const router = express();
 
 router.get("/login", (req, res, next) => {
-  res.render("./account/login.ejs");
+  res.render("./account/login.ejs", { "message": req.flash("message") });
 })
+
+
+router.post("/login", (req, res, next) => {
+  console.log("Login request received:", req.body);  // 追加
+  next();
+}, authenticate());
+//router.post("/login", authenticate());
 
 router.use("/reviews", accountReviews)
 
