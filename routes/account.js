a754aaebeef6332ec964df72fd697b10ba0c508b
kurizz <umozon63313@gmail.com>
2024-07-10 09:21:02 +0900

    認可処理の追加

--- routes/account.js
+++ routes/account.js
@@ -4,7 +4,7 @@ import { authenticate, authorize, PRIVILEGE } from "../lib/security/accesscontro
 
 const router = express();
 
-router.get("/", (req, res, next) => {
+router.get("/", authorize(PRIVILEGE.NORMAL), (req, res, next) => {
   res.render("./account/index.ejs");
 })
 
@@ -12,13 +12,12 @@ router.get("/login", (req, res, next) => {
   res.render("./account/login.ejs", { "message": req.flash("message") });
 })
 
-
 router.post("/login", (req, res, next) => {
   console.log("Login request received:", req.body);  // 追加
   next();
 }, authenticate());
-//router.post("/login", authenticate());
+router.post("/login", authenticate());
 
-router.use("/reviews", accountReviews)
+router.use("/reviews", authorize(PRIVILEGE.NORMAL), accountReviews)
 
 export default router;
